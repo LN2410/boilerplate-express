7fbe9a423d6041d0509907c513f553ba0363297f
Lihle Naphakade <31651036+LN2410@users.noreply.github.com>
2023-01-10 19:10:38 +0200

    create a get request

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require("express");
 let app = express();
 
+app.get("/", function (request, response) {
+  response.console.log("Hello World");
+});
 console.log("Hello World");
 
 module.exports = app;
